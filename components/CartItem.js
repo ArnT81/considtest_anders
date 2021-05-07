7ff6227ab8a44a895bf2459737ff4457b043f10c
ArnT81 <anders_soderberg@hotmail.com>
2021-05-07 15:42:29 +0200

    jesus... never develope in a zoomed out web-browser

--- components/CartItem.js
+++ components/CartItem.js
@@ -10,7 +10,7 @@ const CartItem = ({ redux }) => {
             {redux.map((item, index) => {
                 return (
                     <div key={index} className={styles.cartitem}>
-                        <img src={item.mainImage.url} style={{ height: "100px" }} />
+                        <img src={item.mainImage.url} style={{ height: "60px" }} />
                         <h2>{item.name}</h2>
                         <h2>{item.price}$</h2>
                     </div>
