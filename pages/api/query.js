9f1ca558d58916ba257faec77354af1a173d92df
ArnT81 <anders_soderberg@hotmail.com>
2021-05-07 15:12:47 +0200

    dotenv

--- pages/api/query.js
+++ pages/api/query.js
@@ -1,10 +1,11 @@
 import { GraphQLClient } from 'graphql-request';
+require('dotenv').config()
 
 const query = async ({ query, variables }) => {
     const endpoint = 'https://graphql.datocms.com/';
     const client = new GraphQLClient(endpoint, {
         headers: {
-            authorization: 'Bearer 80d0d8c324a8255230888006fead97'
+            authorization: process.env.BEARER
         }
     })
     return client.request(query, variables);
