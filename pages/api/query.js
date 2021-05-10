454b8b1e752c8ab03a1e888fcdbee76faffa4ad7
ArnT81 <anders_soderberg@hotmail.com>
2021-05-10 19:23:19 +0200

    added bearer again to be able to deploy on vercel

--- pages/api/query.js
+++ pages/api/query.js
@@ -5,7 +5,7 @@ const query = async ({ query, variables }) => {
     const endpoint = 'https://graphql.datocms.com/';
     const client = new GraphQLClient(endpoint, {
         headers: {
-            authorization: process.env.BEARER
+            authorization: 'Bearer 80d0d8c324a8255230888006fead97'
         }
     })
     return client.request(query, variables);
